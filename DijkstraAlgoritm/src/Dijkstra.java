8ad7e282181de82ffc5c77927b544577b48b74e0
Higor Felype <33431875+higor21@users.noreply.github.com>
2018-09-13 23:12:46 -0300

    Update Dijkstra.java

--- DijkstraAlgoritm/src/Dijkstra.java
+++ DijkstraAlgoritm/src/Dijkstra.java
@@ -5,83 +5,96 @@ import java.util.PriorityQueue;
 public class Dijkstra {
         PriorityQueue<Node> naoacomodados;
 	final Graph g; 
-	final int n; 
+	final int n;  // number of nodes
 	final int source; 
 	final int dest; 
         int[] dist;
         int[] pred;
         boolean[] settled; 
-	Fenetre f; 
+	Fenetre f; // graphcs interface
 
 	// construtor
 	Dijkstra(Graph g, int source, int   dest) {
-		this.g = g;
+		this.g = g; // grafo
 		n = g.n;
-		this.source = source;
-		this.dest = dest;
+		this.source = source; // índice do nó de partida
+		this.dest = dest; // índice do nó de destino
                 
-                this.dist = new int[this.n];
-                this.pred = new int[this.n];
+                this.dist = new int[this.n]; // vetor de distncias do nó de partida até o nó da posição inó de partida até o nó da posição i
+                this.pred = new int[this.n]; // vetor dos predecessores de cada nó
                 this.settled = new boolean[this.n];
                 
                 for(int i=0 ; i<this.n; i++){
-                    this.settled[i] = false;
+                    this.settled[i] = false; // inicia todos os nós como não acomodados
+		    // inicia dodos os predecessores com '-1', exceto o predecessor do ponto de partida (source)
                     if(i != this.source) this.pred[i] = -1;
+		    // inicia dodas as distâncias com o valor "infinito", exceto a distância do ponto de partida (source)
                     if(i != this.source) this.dist[i] = Integer.MAX_VALUE;
                 }
                 
-                this.dist[this.source] = 0;
-                this.pred[this.source] = this.source;
+                this.dist[this.source] = 0; // distância do ponto de partida ao próprio ponto de partida
+                this.pred[this.source] = this.source; // inicia como predecessor do ponto de partida como sendo ele mesmo
                 
-                this.naoacomodados = new PriorityQueue<>();
+		// nós que ainda não estão totalmente atualizados (com valores válidos de dist[i] e pred[i])
+                this.naoacomodados = new PriorityQueue<>(); 
 	}
 	
-	// atualizacao da distancia, da prioridade, e do predecessor de um no
+	// atualizacao da distancia, da lista de prioridade, e do predecessor de um no
 	void update(int y, int x) {
-            if(this.dist[y] > this.dist[x] + this.g.value(x, y)){
+	    // verifica qual a menor distância do nó 'x' ao nó 'y' e atualiza a distância da posição 'y'
+            if(this.dist[y] > this.dist[x] + this.g.value(x, y)){  
                 this.dist[y] = this.dist[x] + this.g.value(x, y);
                 this.pred[y] = x;
-                this.naoacomodados.add(new Node(y, this.dist[y]));
-                this.g.drawUnsettledPoint(this.f,y); // ------- LINHA ADD
+                this.naoacomodados.add(new Node(y, this.dist[y])); // adiciona o nó a fila de não acomodados
+                this.g.drawUnsettledPoint(this.f,y); // ------- Desconsidere (só para fins de exibição)
             }
 	}
 	
-	// retorna o próximo nó a ser acomodado
-	int nextNode() { // Pega o primeiro Nó ainda não acomodado
+	// retorna o próximo nó a ser acomodado na lista de 'setted'
+	int nextNode() {
             Node aux;
+		// varre a Fila (naoacomodados) de elementos até encontrar
+		// um nó não acomodado ou até que a Fila fique vazia
             while(!this.naoacomodados.isEmpty()){
-                aux = this.naoacomodados.poll();
+                aux = this.naoacomodados.poll(); // retira o elemento prioritário da 'fila' e o armazena em 'aux'
                 if(!this.settled[aux.id])
-                    return aux.id;
+                    return aux.id; // retorna o índice do primeiro nó encontrado (não acomodado)
             }
-            return -1;
+            return -1; // esse valor indica o fim do programa
         }
 	
-	// uma etapa do algoritmo de Dijkstra
+	// pega o índice do nó a ser acomodado e adiciona todos os nós ligados ao mesmo à 
+	// fila 'naoacomodados'
 	int oneStep() {
-            this.slow();// ------- LINHA ADD
-            int i = this.nextNode(); // Pega o primeiro Nó ainda não acomodado
-            if(i != -1){
+            this.slow();// ------- Desconsidere (só para fins de exibição)
+            int i = this.nextNode(); // índice do nó a ser acomodado, caso o retorno não seja -1
+            if(i != -1){ 
+		// Grafo.successors(int): retorna a lista de posições dos
+		// nós ligados ao nó da posição 'i'
                 ArrayList<Integer> sucessores = this.g.successors(i);
-                this.settled[i] = true;
-                this.g.drawSettledPoint(this.f,i);// ------- LINHA ADD
-                for(int k=0; k<sucessores.size(); k++)
+		
+                this.settled[i] = true; // acomoda o nó 'i'
+                this.g.drawSettledPoint(this.f,i);// ------- Desconsidere (só para fins de exibição)
+                for(int k=0; k<sucessores.size(); k++) // adiciona a lista de successores de 'i' na fila 'naoacomodados'
                    this.update(sucessores.get(k), i);
             }
             return i;
         }
 	
-	// algoritmo de Dijsktra completo
+	// computa a parte principal do algoritmo de Dijsktra
 	int compute() {
-            this.naoacomodados.add(new Node(this.source, 0));
-            int i = -2;
+            this.naoacomodados.add(new Node(this.source, 0)); // adiciona o primeiro nó na fila 'naoacomodados'
+            int i = -2; // define i = -2 para entrar no laço abaixo
             
+	    // roda enquanto não acomodarmos o nó de destino 
             while(!this.settled[this.dest] && i != -1){
                 i = this.oneStep();
             }
             
-            if(i == this.dest)
-                return this.dist[i];
+	    // se o programa não acusou erro, retorna-se a distância mínima
+	    // entre o nó 'source' (pondo de partida) e o nó 'dest' (destino)
+            if(i == this.dest) 
+                return this.dist[i]; // resultado !!
             return -1;
 	}
 	
